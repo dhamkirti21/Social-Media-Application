124255fd6e089e47f03b77e2d6397ce333c5c9e0
Dhamkirti Sisodia <78336507+dhamkirti21@users.noreply.github.com>
2023-05-31 13:08:20 +0530

    Added Post Comment Controller

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -65,9 +65,31 @@ export const likePost = async (req, res) => {
       { likes: post.likes },
       { new: true }
     );
+     
 
     res.status(200).json(updatedPost);
   } catch (err) {
     res.status(404).json({ message: err.message });
   }
 };
+
+export const addComment = async(req,res)=>{
+  try {
+    const { id } = req.params;
+    const { comment } = req.body;
+    const post = await Post.findById(id);
+    
+    post.comments.unshift(comment);
+    
+
+    const updatedPost = await Post.findByIdAndUpdate(
+      id,
+      { comments : post.comments },
+      { new: true }
+    );
+
+    res.status(200).json(updatedPost);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+}
