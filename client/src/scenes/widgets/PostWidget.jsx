dfe0f04082144e259f0a6de4697f3f5270466078
Dhamkirti Sisodia <78336507+dhamkirti21@users.noreply.github.com>
2023-05-31 13:09:34 +0530

    Added Comment Box

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -2,9 +2,10 @@ import {
   ChatBubbleOutlineOutlined,
   FavoriteBorderOutlined,
   FavoriteOutlined,
+  SendOutlined,
   ShareOutlined,
 } from "@mui/icons-material";
-import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
+import { Box, Divider, IconButton, InputBase, Typography, useTheme } from "@mui/material";
 import FlexBetween from "components/FlexBetween";
 import Friend from "components/Friend";
 import WidgetWrapper from "components/WidgetWrapper";
@@ -29,6 +30,8 @@ const PostWidget = ({
   const loggedInUserId = useSelector((state) => state.user._id);
   const isLiked = Boolean(likes[loggedInUserId]);
   const likeCount = Object.keys(likes).length;
+  const [comment , setComment]  = useState("");
+
 
   const { palette } = useTheme();
   const main = palette.neutral.main;
@@ -46,6 +49,25 @@ const PostWidget = ({
     const updatedPost = await response.json();
     dispatch(setPost({ post: updatedPost }));
   };
+
+
+  const sendComment = async() =>{
+     if(comment){
+      const response = await fetch(`http://localhost:5000/posts/${postId}/comment`, {
+        method: "PATCH",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ comment : comment }),
+      });
+      const updatedPost = await response.json();
+      console.log(updatedPost);
+      dispatch(setPost({ post: updatedPost }));
+     }
+  }
+  
+     
 
   return (
     <WidgetWrapper m="2rem 0">
@@ -94,6 +116,21 @@ const PostWidget = ({
       </FlexBetween>
       {isComments && (
         <Box mt="0.5rem">
+         <Box mt="0.3rem" p="0.25rem">
+         {/*COMMENT BOX*/}
+      
+            <FlexBetween
+            backgroundColor="#5A5A5A"
+            borderRadius="9px"
+            gap="3rem"
+            padding="0.1rem 1.5rem"
+          >
+            <InputBase placeholder="Comment Down..." onChange={(e)=>setComment(e.target.value)} />
+            <IconButton onClick={sendComment} >
+              <SendOutlined/>
+            </IconButton>
+          </FlexBetween>
+         </Box>
           {comments.map((comment, i) => (
             <Box key={`${name}-${i}`}>
               <Divider />
